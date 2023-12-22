472545ab0282a0e0a2e64577a1fa5abed7250a82
Marcus Rådell <marcus@radell.net>
2023-12-22 12:09:32 +0100

    add name input field

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,24 @@
 
 import toast, { Toaster } from "react-hot-toast";
 import { Button } from "./button";
+import { useState } from "react";
 
 const notify = () => toast("Hi!");
 
 export default function Home() {
+  const [name, setName] = useState("");
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Toaster />
+      <label htmlFor="name">Name</label>
+      <input
+        className="text-black"
+        id="name"
+        type="text"
+        value={name}
+        onChange={(event) => setName(event.target.value)}
+      />
       <Button onClick={notify}>
         <h1>Say hi!</h1>
         <span>...and you will get a greeting back.</span>
